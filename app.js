98838d14c024586840c36ccec0e6654d23e24e83
vspedr <vspedrazzi@gmail.com>
2016-05-07 22:29:46 -0300

    merged w/ existing project

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+
+var app = express();
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || process.env.OPENSHIFT_INTERNAL_IP || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || '127.0.0.1'
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res) {
+
+
+
+  res.render('index', {
+    title: 'Solar X-Ray Flux Monitor'
+  });
+});
+
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
