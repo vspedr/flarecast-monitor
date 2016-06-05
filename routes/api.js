af4a562ab8f0cd0e811ffe6718d72f945a9ec2e7
Mateus Andrade <mat.andrade@live.com>
2016-06-05 19:09:48 -0300

    package json fixed, api changed & sending data to front-end

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var request = require("request");
 var fs = require("fs");
 var Client = require("ftp");
 var builder = require("../lib/builders.js");
@@ -26,6 +25,43 @@ router.get('/', function(req, res) {
                 builder.stringParser(dataFile, function(dataParsed) {
                     //important data starts in position "99"!
                     builder.jsonBuilder(dataParsed, function(json) {
+                        res.send(json);
+                    });
+                });
+                //res.send(dataFile);
+            });
+
+            stream.once('close', function() {
+                c.end();
+            });
+            //stream.pipe(fs.createWriteStream('foo.local-copy.txt'));
+        });
+    });
+    c.connect({
+        'host': 'ftp.swpc.noaa.gov',
+    });
+});
+
+router.get('/long', function(req, res) {
+    var c = new Client();
+
+    //res.io.emit("text", "users");
+
+    c.on('ready', function() {
+        c.get('/pub/lists/xray/Gp_xr_1m.txt', function(err, stream) {
+            if (err) throw err;
+            const chunks = [];
+
+            stream.on("data", function(chunk) {
+                chunks.push(chunk);
+            });
+
+            stream.on("end", function() {
+                var dataFile = (Buffer.concat(chunks)).toString();
+                //console.log(dataFile);
+                builder.stringParser(dataFile, function(dataParsed) {
+                    //important data starts in position "99"!
+                    builder.longObject(dataParsed, function(json) {
                         res.send(json);
                     });
                 });
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var request = require("request");
 
 /* GET home page. */
 router.get('/', function(req, res) {
     res.io.emit("data", "hey, it's me! I can handle data and display it into graphic!! o/"); //I will use socket.io to make graphic realtime
-    res.render('index', {
-        title: 'Solar X-Ray Flux Monitor'
+    request('http://localhost:8080/api/long', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          body = JSON.parse(body);
+          console.log(body);
+            res.render('index', {
+                title: 'Solar X-Ray Flux Monitor',
+                data: body
+            });
+        }
     });
 });
 
